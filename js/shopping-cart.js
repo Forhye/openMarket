7a23f973c491a4f15423d32a48c58106e04244ed
Forhye <onlyh4425@gmail.com>
2024-08-05 21:54:29 +0900

    ✅ add : 서버 상품 데이터 가져와서 장바구니 템플릿에 맞게 정렬 - fix예정 : $ul2 수정

---
+++ js/shopping-cart.js
@@ -0,0 +1,44 @@
+const $section = document.querySelector(".cart--list");
+
+async function getData() {
+  try {
+    const res = await fetch("https://openmarket.weniv.co.kr/products/");
+    const json = await res.json();
+    data = json.results;
+
+    data.forEach((item) => {
+      const $ul = document.createElement("ul");
+      const $ul2 = document.createElement("ul");
+
+      $ul.className = "cart--list__product";
+
+      $ul.innerHTML = `
+        <li><input type="radio" name="check" id="cart--totalCehck"></li>
+        <li><img src="${item.image}"></li>
+        <li>
+            <button type="button"></button>
+            <p>1</p><button type="button"></button>
+        </li>
+        <li>
+            <p class="cart--price">${item.price}원</p>
+            <button type="submit">주문하기</button>
+        </li>
+        <li><button type="button"></button></li>
+      `;
+
+      $ul2.innerHTML = `
+      <li>${item.store_name}</li>
+      <li>${item.product_name}</li>
+      <li>${item.price}원</li>
+      <li>택배배송 / 무료배송</li>
+      `;
+
+      $section.appendChild($ul);
+      $ul.appendChild($ul2);
+    });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getData();
