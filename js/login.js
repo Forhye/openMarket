7cf3e16c62eb343b85d8c7e1a12da8dfc89fcd04
Forhye <onlyh4425@gmail.com>
2024-08-06 13:09:08 +0900

    ✅ add : 아이디/비밀번호가 일치하지 않았을 때 기능 추가 + 경고문구를 담고 있는 p 태그가 나타나는 기능 + 비밀번호 입력창이 비워지면서 focus 되는 기능

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 const $loginBtn = document.querySelector("button");
 const $inputId = document.querySelector("input#user--id");
 const $inputPw = document.querySelector("input#user--pw");
+let loginType = "BUYER";
+const $p = document.querySelector(".login__failed");
 
 document.addEventListener("DOMContentLoaded", () => {
   const $selectedType = document.querySelectorAll(".login--type");
@@ -11,6 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
         // 현재 요소의 클래스 토글
         type.classList.remove("off");
         type.classList.add("on");
+
+        typeSeach(type.innerText);
 
         // 형제 요소의 클래스 토글
         for (const sibling of $selectedType) {
@@ -23,23 +27,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-// $selectedType.addEventListener("click", () => {
-// });
 
-let typeSeach = (e) => {
+const typeSeach = (e) => {
   if (e === "구매회원 로그인") {
-    longinType = "BUYER";
+    loginType = "BUYER";
   } else {
-    longinType = "SELLER";
+    loginType = "SELLER";
   }
 };
-typeSeach($selectedType.innerText);
 
 $loginBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   // form의 기본 제출을 막지 못해서 주소창에 자꾸 id랑pw가 떠서 기본 제출 동작을 막아줌
   const username = $inputId.value;
   const password = $inputPw.value;
+
   try {
     res = await fetch("https://openmarket.weniv.co.kr/accounts/login/", {
       method: "POST",
@@ -49,17 +51,19 @@ $loginBtn.addEventListener("click", async (e) => {
       body: JSON.stringify({
         username: username,
         password: password,
-        login_type: longinType,
+        login_type: loginType,
       }),
     });
-    console.log(res.status);
     if (res.ok) {
       // response 가 200 = res.status === 200
       window.location.href = "product-list.html";
     } else {
       // todo
       // 비밀번호 입력창에 focus이벤트가 발생하고 빈칸이 됩니다.
-      alert("잘못된 회원정보입니다.");
+      $p.style.display = "block";
+
+      $inputPw.focus();
+      $inputPw.value = null;
     }
   } catch (error) {
     console.error(error);
